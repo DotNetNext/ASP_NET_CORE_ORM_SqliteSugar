0f193475ae6c7c4ef43ead9a8260bd03ad12fe06
sunkaixuan <610262374@qq.com>
2016-10-26 02:29:16 +0800

    3.1.1.4

--- SQLiteSugarForCore/src/NewTest/Properties/AssemblyInfo.cs
+++ SQLiteSugarForCore/src/NewTest/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("52f4c569-4543-44df-9552-6c6c5e359c76")]
-[assembly: AssemblyVersion("3.1.1.0")]
-[assembly: AssemblyFileVersion("3.1.1.0")]
+[assembly: AssemblyVersion("3.1.1.4")]
+[assembly: AssemblyFileVersion("3.1.1.4")]
